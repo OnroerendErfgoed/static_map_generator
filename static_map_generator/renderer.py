7870d0b9385a97eb46f51112e1fca3773dfb2e5f
Cedrik Vanderhaegen <cedrik.vanderhaegen@geosolutions.be>
2016-06-30 08:51:45 +0200

    create geojson layer in memory

--- static_map_generator/renderer.py
+++ static_map_generator/renderer.py
@@ -1,39 +1,41 @@
-import os
-import warnings
+# -*- coding: utf-8 -*-
+"""
+Adapters to help render map layers
+"""
+# import os
 from abc import ABCMeta, abstractmethod
 import json
-
 from pyramid.httpexceptions import HTTPNotFound
-from requests import ConnectionError
+from requests.packages.urllib3.connection import ConnectionError
 import requests
-
 import mapnik
-from wand.color import Color
-from wand.display import display
-from wand.image import Image
-from wand.image import Font
-from static_map_generator.utils import merge_dicts, convert_wkt_to_geojson, position_figure, define_scale_number
+# from wand.color import Color
+# from wand.display import display
+# from wand.image import Image
+# from wand.image import Font
+from static_map_generator.utils import merge_dicts
+# from static_map_generator.utils import convert_wkt_to_geojson, position_figure, define_scale_number
 
 
-class Renderer():
+class Renderer(object):
     __metaclass__ = ABCMeta
 
     @staticmethod
-    def factory(type):
-        if type == "wms":
+    def factory(layer_type):
+        if layer_type == "wms":
             return WmsRenderer()
-        elif type == "wkt":
-            return WktRenderer()
-        elif type == "geojson":
+        # elif layer_type == "wkt":
+        #     return WktRenderer()
+        elif layer_type == "geojson":
             return GeojsonRenderer()
-        elif type == "text":
-            return TextRenderer()
-        elif type == "logo":
-            return LogoRenderer()
-        elif type == "scale":
-            return ScaleRenderer()
-        elif type == "legend":
-            return LegendRenderer()
+        # elif layer_type == "text":
+        #     return TextRenderer()
+        # elif layer_type == "logo":
+        #     return LogoRenderer()
+        # elif layer_type == "scale":
+        #     return ScaleRenderer()
+        # elif layer_type == "legend":
+        #     return LegendRenderer()
         else:
             return DefaultRenderer()
 
@@ -91,7 +93,13 @@ class GeojsonRenderer(Renderer):
         r.symbols.append(point_symbolizer)
         s.rules.append(r)
         m.append_style('My Style', s)
-        ds = mapnik.Ogr(string=json.dumps(kwargs['geojson']), layer='OGRGeoJSON')
+        # ds = mapnik.Ogr(string=json.dumps(kwargs['geojson']), layer='OGRGeoJSON')
+        testgeojson = { "type": "Feature", "properties": {}, "geometry": {}}
+        testgeojson['geometry'] = kwargs['geojson']
+        ds = mapnik.MemoryDatasource()
+        context = mapnik.Context()
+        feature = mapnik.Feature.from_geojson(json.dumps(testgeojson), context)
+        ds.add_feature(feature)
         layer = mapnik.Layer('wkt', '+init=epsg:' + str(kwargs['epsg']))
         layer.datasource = ds
         layer.styles.append('My Style')
@@ -104,75 +112,75 @@ class GeojsonRenderer(Renderer):
         return "geojson"
 
 
-class WktRenderer(Renderer):
-    def render(self, **kwargs):
-        kwargs['geojson'] = convert_wkt_to_geojson(kwargs['wkt'])
-        GeojsonRenderer().render(**kwargs)
-
-    def type(self):
-        return "wkt"
-
-
-class TextRenderer(Renderer):
-    def render(self, **kwargs):
-
-        with Image(width=kwargs['width'],
-                   height=kwargs['height']) as image:
-            font = Font(path='/Library/Fonts/Verdana.ttf', size=kwargs['font_size'], color=Color(kwargs['text_color']))
-            image.caption(kwargs['text'], left=0, top=0,
-                          font=font, gravity=kwargs['gravity'])
-            image.save(filename=kwargs['filename'])
-
-    def type(self):
-        return "text"
-
-
-class LogoRenderer(Renderer):
-    def render(self, **kwargs):
-
-        response = requests.get(kwargs['url'], stream=True)
-        with Image(blob=response.content) as img:
-            img.resize(width=kwargs['imagewidth'], height=kwargs['imageheight'])
-            img.transparentize(1 - kwargs['opacity'])
-            position_figure(kwargs['width'], kwargs['height'], img, kwargs['gravity'], kwargs['offset'],  kwargs['filename'])
-
-    def type(self):
-        return "logo"
-
-
-class ScaleRenderer(Renderer):
-        #todo: this is just some test implementation!
-    def render(self, **kwargs):
-        if kwargs['epsg']!= 31370:
-            raise NotImplementedError("This method is not yet implemented for epsg other than 31370")
-
-        here = os.path.abspath(os.path.dirname(__file__))
-        path = os.path.join(here, 'fixtures/scalebar.png')
-        with Image(filename=path) as scale_img:
-            # scale_number = str((kwargs['bbox'][2]- kwargs['bbox'][0])/kwargs['divid']) + ' m'
-            # scalebar_width = int(kwargs['width']/kwargs['divid'])
-            # scalebar_height = (scale_img.height*scalebar_width/scale_img.width)
-            # scale_img.resize(width=scalebar_width, height = scalebar_height)
-            scale_number = define_scale_number(kwargs['bbox'][2]- kwargs['bbox'][0], kwargs['width'], kwargs['imagewidth'])
-            scale_img.resize(width=kwargs['imagewidth'], height = kwargs['imageheight'])
-            scale_img.transparentize(1 - kwargs['opacity'])
-            font = Font(path='/Library/Fonts/Verdana.ttf', size=kwargs['font_size'], color=Color('#000000'))
-            scale_img.caption(scale_number, left=0, top=0,
-                          font=font, gravity='center')
-            position_figure(kwargs['width'], kwargs['height'], scale_img, kwargs['gravity'], kwargs['offset'], kwargs['filename'])
-
-
-    def type(self):
-        return "scale"
-
-
-class LegendRenderer(Renderer):
-    def render(self, **kwargs):
-        raise NotImplementedError("This method is not yet implemented")
-
-    def type(self):
-        return "legend"
-
+# class WktRenderer(Renderer):
+#     def render(self, **kwargs):
+#         kwargs['geojson'] = convert_wkt_to_geojson(kwargs['wkt'])
+#         GeojsonRenderer().render(**kwargs)
+#
+#     def type(self):
+#         return "wkt"
+#
+#
+# class TextRenderer(Renderer):
+#     def render(self, **kwargs):
+#
+#         with Image(width=kwargs['width'],
+#                    height=kwargs['height']) as image:
+#             font = Font(path='/Library/Fonts/Verdana.ttf', size=kwargs['font_size'], color=Color(kwargs['text_color']))
+#             image.caption(kwargs['text'], left=0, top=0,
+#                           font=font, gravity=kwargs['gravity'])
+#             image.save(filename=kwargs['filename'])
+#
+#     def type(self):
+#         return "text"
+#
+#
+# class LogoRenderer(Renderer):
+#     def render(self, **kwargs):
+#
+#         response = requests.get(kwargs['url'], stream=True)
+#         with Image(blob=response.content) as img:
+#             img.resize(width=kwargs['imagewidth'], height=kwargs['imageheight'])
+#             img.transparentize(1 - kwargs['opacity'])
+#             position_figure(kwargs['width'], kwargs['height'], img, kwargs['gravity'], kwargs['offset'],  kwargs['filename'])
+#
+#     def type(self):
+#         return "logo"
+#
+#
+# class ScaleRenderer(Renderer):
+#         #todo: this is just some test implementation!
+#     def render(self, **kwargs):
+#         if kwargs['epsg']!= 31370:
+#             raise NotImplementedError("This method is not yet implemented for epsg other than 31370")
+#
+#         here = os.path.abspath(os.path.dirname(__file__))
+#         path = os.path.join(here, 'fixtures/scalebar.png')
+#         with Image(filename=path) as scale_img:
+#             # scale_number = str((kwargs['bbox'][2]- kwargs['bbox'][0])/kwargs['divid']) + ' m'
+#             # scalebar_width = int(kwargs['width']/kwargs['divid'])
+#             # scalebar_height = (scale_img.height*scalebar_width/scale_img.width)
+#             # scale_img.resize(width=scalebar_width, height = scalebar_height)
+#             scale_number = define_scale_number(kwargs['bbox'][2]- kwargs['bbox'][0], kwargs['width'], kwargs['imagewidth'])
+#             scale_img.resize(width=kwargs['imagewidth'], height = kwargs['imageheight'])
+#             scale_img.transparentize(1 - kwargs['opacity'])
+#             font = Font(path='/Library/Fonts/Verdana.ttf', size=kwargs['font_size'], color=Color('#000000'))
+#             scale_img.caption(scale_number, left=0, top=0,
+#                           font=font, gravity='center')
+#             position_figure(kwargs['width'], kwargs['height'], scale_img, kwargs['gravity'], kwargs['offset'], kwargs['filename'])
+#
+#
+#     def type(self):
+#         return "scale"
+#
+#
+# class LegendRenderer(Renderer):
+#     def render(self, **kwargs):
+#         raise NotImplementedError("This method is not yet implemented")
+#
+#     def type(self):
+#         return "legend"
+#
 
 class DefaultRenderer(Renderer):
     def render(self, **kwargs):
